a995d61b0a6add6692289d87cd4b43454753f947
Imaad Uddin <imaaduddin@icloud.com>
2021-02-09 23:57:42 -0600

    worked on index.js file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,54 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
 
 const App = () => {
-  return <div>Hi there!</div>
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.image()} />
+        </a>
+        <div classname="content">
+          <a href="/" classname="author">
+            Sasuke
+          </a>
+          <div classname="metadata">
+            <span classname="date">Today at 11:30pm</span>
+          </div>
+          <div classname="text">Nice blog post!</div>
+        </div>
+      </div>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.image()} />
+        </a>
+        <div classname="content">
+          <a href="/" classname="author">
+            Sasuke
+          </a>
+          <div classname="metadata">
+            <span classname="date">Today at 11:30pm</span>
+          </div>
+          <div classname="text">Nice blog post!</div>
+        </div>
+      </div>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.image()} />
+        </a>
+        <div classname="content">
+          <a href="/" classname="author">
+            Sasuke
+          </a>
+          <div classname="metadata">
+            <span classname="date">Today at 11:30pm</span>
+          </div>
+          <div classname="text">Nice blog post!</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
