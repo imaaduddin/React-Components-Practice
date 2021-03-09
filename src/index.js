049749afd2fd1b4dd15b61a9b94505e533ce4dda
Imaad Uddin <imaaduddin@icloud.com>
2021-03-09 16:35:40 -0600

    connecting files

--- src/index.js
+++ src/index.js
@@ -2,37 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 
+import CommentDetail from "./CommentDetail";
+
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.image()} />
-        </a>
-        <div classname="content">
-          <a href="/" classname="author">
-            Sasuke
-          </a>
-          <div classname="metadata">
-            <span classname="date">Today at 11:30pm</span>
-          </div>
-          <div classname="text">Nice blog post!</div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.image()} />
-        </a>
-        <div classname="content">
-          <a href="/" classname="author">
-            Sasuke
-          </a>
-          <div classname="metadata">
-            <span classname="date">Today at 11:30pm</span>
-          </div>
-          <div classname="text">Nice blog post!</div>
-        </div>
-      </div>
+      <CommentDetail />
     </div>
   );
 };
